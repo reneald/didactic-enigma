3b6cb5f2ba9c70dd77ad0dc9a1ab55d5a33f9260
reneald <reneald@protonmail.com>
2022-11-21 14:26:24 +0100

    some basic REST calls

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,31 @@
 const config = {
     backendBaseUri: "http://localhost:8080",
-    createEmptyCharacterUri: "/character/new"
+    createCharacterUri: "/character/new/",
+    getAllTypesUri: "/type/all"
 }
 
-function createEmptyCharacter() {
-    fetch(`${config.backendBaseUri}${config.createEmptyCharacterUri}`, {
-        method: "POST"
+function createCharacter() {
+    const name = document.getElementById("charName").value;
+    const character = {
+        name: name
+    }
+    fetch(`${config.backendBaseUri}${config.createCharacterUri}`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify(character)
     })
     .then(response => response.json())
+    .then(response => alert(response.id))
+    .catch(error => console.log(error));
+}
+
+function showTypes() {
+    fetch(`${config.backendBaseUri}${config.getAllTypesUri}`, {
+        method: "GET",
+        mode: "no-cors"
+    }).then(response => response.json())
     .then(response => alert(response))
-    .catch(error => console.log(error))
+    .catch(error => console.log(error));
 }
